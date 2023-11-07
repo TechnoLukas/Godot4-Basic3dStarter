c69236865db028681a7b176a1e2b62009eec7e11
technolukas <110934679+TechnoLukas@users.noreply.github.com>
2023-11-07 23:26:47 +0100

    simplified

--- character_movement.gd
+++ character_movement.gd
@@ -1,27 +1,22 @@
-extends CharacterBody3D
-
-# ------------- CHARACTER VIEWPORT -------------
-
-## --- VIEWPORT VARIABLES ---
-@onready var fp_camera = $FP_Camera
-var camera
-
-func ready_character_viewport():
-	fp_camera.current = true
-	camera=fp_camera
-	Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
+extends Node3D
 
 # ------------- CHARACTER MOVEMENT -------------
 
-## --- MOVEMENT VARIABLES ---
+@onready var PLAYER : CharacterBody3D = get_parent()
+
+## --- VARIABLES ---
 var speed = 5.0
 var jump_velocity = 4.5
 var horizontal_acceleration = 30
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var camera = $camera
+
+func _ready():
+	Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * .005)
+		PLAYER.rotate_y(-event.relative.x * .005)
 		camera.rotate_x(-event.relative.y * .005)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
 		print(camera)
@@ -35,12 +30,12 @@ func _unhandled_key_input(event):
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	if not PLAYER.is_on_floor():
+		PLAYER.velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y += jump_velocity
+	if Input.is_action_just_pressed("ui_accept") and PLAYER.is_on_floor():
+		PLAYER.velocity.y += jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -49,19 +44,14 @@ func _physics_process(delta):
 	input_dir.x = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").x
 	input_dir.y = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").y
 	input_dir=input_dir.normalized()
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (PLAYER.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	direction *= speed
-	velocity.x = move_toward(velocity.x,direction.x, horizontal_acceleration * delta)
-	velocity.z = move_toward(velocity.z,direction.z, horizontal_acceleration * delta)
+	PLAYER.velocity.x = move_toward(PLAYER.velocity.x,direction.x, horizontal_acceleration * delta)
+	PLAYER.velocity.z = move_toward(PLAYER.velocity.z,direction.z, horizontal_acceleration * delta)
 
 	var angle=5
 	var t = delta * 6
-	rotation_degrees=rotation_degrees.lerp(Vector3(input_dir.normalized().y*angle,rotation_degrees.y,-input_dir.normalized().x*angle),t)
+	PLAYER.rotation_degrees=PLAYER.rotation_degrees.lerp(Vector3(input_dir.normalized().y*angle,PLAYER.rotation_degrees.y,-input_dir.normalized().x*angle),t)
 	
-	move_and_slide()
-	force_update_transform()
-	
-
-# ------------- GROUPED FUNCTIONS -------------
-func _ready():
-	ready_character_viewport()
+	PLAYER.move_and_slide()
+	PLAYER.force_update_transform()
