52b77bb4b3e7a43541afc98a9b74f435ea072dd8
technolukas <110934679+TechnoLukas@users.noreply.github.com>
2023-11-08 22:45:10 +0100

    fixed, and simplified

--- character_new.gd
+++ character_new.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 # ------------- CHARACTER VIEWPORT -------------
 
 ## --- VIEWPORT VARIABLES ---
-@onready var fp_camera = $FP_Camera
+@onready var fp_camera = $Camera3D
 var camera
 
 func ready_character_viewport():
@@ -24,7 +24,6 @@ func _unhandled_input(event):
 		rotate_y(-event.relative.x * .005)
 		camera.rotate_x(-event.relative.y * .005)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
-		print(camera)
 
 func _unhandled_key_input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -46,8 +45,12 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Vector3.ZERO
 	var movetoward = Vector3.ZERO
-	input_dir.x = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").x
-	input_dir.y = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").y
+	input_dir.x = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_forward", "move_backward")).x 
+	input_dir.y = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_forward", "move_backward")).y
+	 
+	#input_dir.x = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").x   # <-- this has weird glithes on web. Looks like it is getting stuck on value, and player keeps moving while person is touching nothing.
+	#input_dir.y = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").y   # <-- this has weird glithes on web. Looks like it is getting stuck on value, and player keeps moving while person is touching nothing.
+	
 	input_dir=input_dir.normalized()
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	direction *= speed
