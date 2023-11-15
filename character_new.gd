88370e2223ed0c9b20bb743d79332d72df06da53
technolukas <110934679+TechnoLukas@users.noreply.github.com>
2023-11-15 18:53:12 +0100

    update

--- character_new.gd
+++ character_new.gd
@@ -1,17 +1,10 @@
 extends CharacterBody3D
 
-# ------------- CHARACTER VIEWPORT -------------
-
+# ------------- CHARACTER -------------
 ## --- VIEWPORT VARIABLES ---
 @onready var fp_camera = $Camera3D
 var camera
-
-func ready_character_viewport():
-	fp_camera.current = true
-	camera=fp_camera
-	Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
-
-# ------------- CHARACTER MOVEMENT -------------
+var mouse_is_active = false
 
 ## --- MOVEMENT VARIABLES ---
 var speed = 5.0
@@ -19,39 +12,70 @@ var jump_velocity = 4.5
 var horizontal_acceleration = 30
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	fp_camera.current = true
+	camera=fp_camera
+	#Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
+	#Input.mouse_mode=Input.MOUSE_MODE_HIDDEN
+	mouse_is_active=false
+
 func _unhandled_input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and mouse_is_active:
+		print(event.relative.x)
 		rotate_y(-event.relative.x * .005)
 		camera.rotate_x(-event.relative.y * .005)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
 
 func _unhandled_key_input(event):
-	if Input.is_action_just_pressed("ui_cancel"):
-		if Input.mouse_mode==Input.MOUSE_MODE_CAPTURED: 
-			Input.mouse_mode=Input.MOUSE_MODE_VISIBLE
+	if Input.is_action_just_pressed("mouse_active"):
+		if OS.get_name() == "Web":
+			if mouse_is_active:
+				print("request unlock")
+				JavaScriptBridge.eval("""
+				var element = document.pointerLockElement;
+				console.log(element);
+				document.exitPointerLock();
+				""")
+				mouse_is_active=false
+			else:
+				JavaScriptBridge.eval("""
+				var element = document.pointerLockElement;
+				console.log(element);
+				document.body.requestPointerLock();
+				""")
+				mouse_is_active=true
 		else:
-			Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
+			if mouse_is_active:
+				Input.mouse_mode=Input.MOUSE_MODE_VISIBLE
+				mouse_is_active=false
+			else:
+				Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
+				mouse_is_active=true
 
-func _physics_process(delta):
+func _process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and mouse_is_active:
 		velocity.y += jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Vector3.ZERO
-	var movetoward = Vector3.ZERO
-	input_dir.x = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_forward", "move_backward")).x 
-	input_dir.y = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_forward", "move_backward")).y
-	 
+	var input_dir = Vector2.ZERO
+	if mouse_is_active:
+		print(Input.is_action_pressed("move_forward")," ",Input.is_action_pressed("move_backward")," ",Input.is_action_pressed("move_left")," ",Input.is_action_pressed("move_right"))
+		input_dir.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+		input_dir.y = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")	
+	
+	#input_dir.x = Input.get_axis("move_left", "move_right")
+	#input_dir.y = Input.get_axis("move_forward", "move_backward")
+		 
 	#input_dir.x = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").x   # <-- this has weird glithes on web. Looks like it is getting stuck on value, and player keeps moving while person is touching nothing.
 	#input_dir.y = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").y   # <-- this has weird glithes on web. Looks like it is getting stuck on value, and player keeps moving while person is touching nothing.
 	
-	input_dir=input_dir.normalized()
+	#input_dir=input_dir.normalized()
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	direction *= speed
 	velocity.x = move_toward(velocity.x,direction.x, horizontal_acceleration * delta)
@@ -63,8 +87,3 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	force_update_transform()
-	
-
-# ------------- GROUPED FUNCTIONS -------------
-func _ready():
-	ready_character_viewport()
